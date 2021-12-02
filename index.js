edda367138f225cbd6f00226c51305c57381fdae
Angie Munoz <angiepmunoz@gmail.com>
2021-12-02 16:27:03 -0500

    updated to arrow function

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-function addAllNumbers(...nums) {
+const addAllNumbers = (...nums) => {
     if(!nums.every((num) => typeof num === "number")){
         throw "All arguments must be numbers";
     }
-
 
     return nums.reduce((a,b) => a + b);
 }
 
 console.log(addAllNumbers(1,2,3))
+console.log(addAllNumbers(1,"2",3))
