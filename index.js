d071702b480f7a9ad0a81a86322b0497a7656117
Colin Jaffe <balloonasaurus@gmail.com>
2021-12-02 16:10:26 -0500

    Refactors if statement.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function fn(...c) {
-    if(!c.every((a) => Number.isNumber(a)))
+    if(!c.every((a) => Number.isNumber(a))) {
         throw "All arguments must be numbers";
+    }
     
     return c.reduce((a,b) => a + b);
 }
